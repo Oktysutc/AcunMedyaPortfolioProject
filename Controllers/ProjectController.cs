3a3668d2300fa944845b26463d062e98cba49a16
Oktay Sütçü <oktysutc@gmail.com>
2025-01-15 20:15:56 +0300

    .

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,35 @@
+﻿using AcunMedyaPortfolioProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMedyaPortfolioProject.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        PortfolioDbEntities db=new PortfolioDbEntities();
+        public ActionResult Index()
+        {
+            var projects = db.Projects.ToList();
+            return View(projects);
+        }
+        [HttpGet]
+        public ActionResult CreateProject()
+        {
+            var categories=db.Categories.ToList();
+            var list = new SelectList(categories,"Id","Name");
+            ViewBag.categories = list;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateProject(Project project)
+        {
+            db.Projects.Add(project);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
